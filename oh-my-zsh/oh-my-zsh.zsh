6b0b7abab70ce33cbbe7f790bfdf5dc3ab6c690e
Joseph Muller <j.muller@bbk.ac.uk>
2023-01-18 22:29:46 +0000

    Add plugin for virtualenv and change theme

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -7,13 +7,14 @@ export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="kphoen"
+# ZSH_THEME="kphoen"
+ZSH_THEME="pygmalion-virtualenv"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-autosuggestions zsh-vi-mode virtualenvwrapper)
+plugins=(git zsh-autosuggestions zsh-vi-mode virtualenv virtualenvwrapper)
 
 source "$ZSH/oh-my-zsh.sh"
 
