53740419906943a9dbe063815da37584b3035dab
Joseph Muller <j.muller@bbk.ac.uk>
2023-07-15 11:33:56 +0100

    Experiment with neovim and CoC extensions

---
+++ nvim/install.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+sudo apt-get install ripgrep
+sudo apt-get install fzf
+
+exit 0
--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 if [ ! -d ~/.vim ]; then
   sudo apt-get install vim
-  if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 fi
 
