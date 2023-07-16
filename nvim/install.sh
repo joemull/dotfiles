6f7fbcdf52b44cb6e7999ed0411216340b085a51
Joseph Muller <j.muller@bbk.ac.uk>
2023-07-16 22:23:47 +0100

    Configure COC

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,5 +1,7 @@
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
+ln -s ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
 sudo apt-get install ripgrep
 sudo apt-get install fzf
 
