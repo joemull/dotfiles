4e7e33401d157cbfb6d9cb35df460f3d71511263
Joseph Muller <j.muller@bbk.ac.uk>
2024-04-07 20:46:12 +0100

    Install node

--- javascript/install.sh
+++ javascript/install.sh
@@ -1,7 +1,3 @@
-if test $(which node)
-then
-  if test $(which npm)
-  then
-    npm install -g standard
-  fi
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.zshrc
+nvm install v20
