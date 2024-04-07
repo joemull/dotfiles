e414a399c28841b92bcec39f146b2bafd471592c
Joseph Muller <j.muller@bbk.ac.uk>
2024-04-07 20:54:02 +0100

    Rethink python versions and environments

--- python/install.sh
+++ python/install.sh
@@ -1,4 +1,16 @@
-if test $(which apt-get)
+sudo apt update
+sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install python3-pip
+
+# Set up pyenv to get python 3.10
+curl https://pyenv.run | bash
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+if test ! $(which python3.10)
 then
-  sudo apt-get install flake8
+  pyenv install 3.10
 fi
